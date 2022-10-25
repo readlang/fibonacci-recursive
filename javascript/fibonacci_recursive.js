88b809eb0901aa0d402bc2749498428ccbc46bd8
Read Langworthy <rlangworthy@gmail.com>
2022-10-25 13:08:09 -0700

    done

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,11 @@
 function fibonacci(n) {
   // type your code here
+
+  if (n===0) return 0;
+  if (n===1) return 1;
+
+  return ( fibonacci(n-1) + fibonacci(n-2) )
+
 }
 
 if (require.main === module) {
@@ -10,12 +16,17 @@ if (require.main === module) {
   console.log("");
 
   console.log("Expecting: 1");
+  console.log("=>", fibonacci(1));
+
+  console.log("");
+
+  console.log("Expecting: 1");
   console.log("=>", fibonacci(2));
 
   console.log("");
 
-  console.log("Expecting: 55");
-  console.log("=>", fibonacci(10));
+  console.log("Expecting: 3");
+  console.log("=>", fibonacci(4));
 }
 
 module.exports = fibonacci;
